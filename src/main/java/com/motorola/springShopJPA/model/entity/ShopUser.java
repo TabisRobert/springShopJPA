88529da6cd5e04753b429c4a9d4e60679b49e09b
Robert Tabiś <robert.tabis@hotmail.com>
2021-09-21 20:13:05 +0200

    User renamed to ShopUser because user is reserved keyword in PostgreSQL

---
+++ src/main/java/com/motorola/springShopJPA/model/entity/ShopUser.java
@@ -0,0 +1,45 @@
+package com.motorola.springShopJPA.model.entity;
+
+
+
+import com.motorola.springShopJPA.model.enums.UserRole;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+
+@Entity
+public class ShopUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotNull
+    @Size(max = 50)
+    private String email;
+
+    @NotNull
+    @Size(max = 50)
+    private String password;
+
+    @NotNull
+    private UserRole role;
+
+    @OneToMany(mappedBy = "shopUser")
+    private List<PurchaseOrder> purchaseOrderList;
+
+    public ShopUser() {
+    }
+
+    public ShopUser(Long id, @NotNull @Size(max = 50) String email, @NotNull @Size(max = 50) String password, @NotNull UserRole role, List<PurchaseOrder> purchaseOrderList) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.purchaseOrderList = purchaseOrderList;
+    }
+}
