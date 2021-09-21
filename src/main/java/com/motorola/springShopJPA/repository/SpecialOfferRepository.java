7d4627baf58821b3c8232d3945de6d509c277b08
Robert Tabiś <robert.tabis@hotmail.com>
2021-09-21 23:13:49 +0200

    SpecialOfferRepository interface created (extends CrudRepository)

---
+++ src/main/java/com/motorola/springShopJPA/repository/SpecialOfferRepository.java
@@ -0,0 +1,7 @@
+package com.motorola.springShopJPA.repository;
+
+import com.motorola.springShopJPA.model.entity.SpecialOffer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialOfferRepository extends CrudRepository<SpecialOffer, Long> {
+}
