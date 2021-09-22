906b0f4ddd45f73b2a1498765dc2807c3aaf7b3c
Robert Tabiś <robert.tabis@hotmail.com>
2021-09-22 23:28:36 +0200

    ModelMapper created - it will be needed for creating Dto's and mapping them to entities
    PasswordEncoder created - for creating encoded passwords

---
+++ src/main/java/com/motorola/springShopJPA/configuration/SpringConfiguration.java
@@ -0,0 +1,22 @@
+package com.motorola.springShopJPA.configuration;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringConfiguration {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+}
