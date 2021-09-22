8d4649170122a36961602aca12936256d9e53167
Robert Tabiś <robert.tabis@hotmail.com>
2021-09-22 23:29:33 +0200

    business logic for creating newe users and saving them in database

--- src/main/java/com/motorola/springShopJPA/repository/ShopUserRepository.java
+++ src/main/java/com/motorola/springShopJPA/repository/ShopUserRepository.java
@@ -2,6 +2,9 @@ package com.motorola.springShopJPA.repository;
 
 import com.motorola.springShopJPA.model.entity.ShopUser;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShopUserRepository extends CrudRepository<ShopUser, Long> {
+    ShopUser findByEmail(String username);
 }
---
+++ src/main/java/com/motorola/springShopJPA/service/ShopUserService.java
@@ -0,0 +1,85 @@
+package com.motorola.springShopJPA.service;
+
+import com.motorola.springShopJPA.model.dto.ShopUserDto;
+import com.motorola.springShopJPA.model.entity.ShopUser;
+import com.motorola.springShopJPA.model.enums.UserRole;
+import com.motorola.springShopJPA.repository.ShopUserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ShopUserService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private ShopUserRepository shopUserRepository;
+
+    public void createNewUser(ShopUserDto shopUserDto) {
+        prepareUserRole(shopUserDto);
+        validateUser(shopUserDto);
+        String passwordHash = bCryptPasswordEncoder.encode(shopUserDto.getPassword());
+        ShopUser shopUser = modelMapper.map(shopUserDto, ShopUser.class);
+        shopUser.setPassword(passwordHash);
+        shopUser.setEnabled(true);
+        shopUserRepository.save(shopUser);
+    }
+
+    public List<ShopUserDto> getAllUsers(){
+        ShopUser loggedUser = findCurrentlyLoggedUser();
+        List<ShopUser> users = (List<ShopUser>) shopUserRepository.findAll();
+        List<ShopUserDto> userDtos = new ArrayList<>();
+        for (ShopUser shopUser: users){
+            if (shopUser.getId().equals(loggedUser.getId())){
+                continue;
+            }
+            ShopUserDto userDto = modelMapper.map(shopUser, ShopUserDto.class);
+            userDtos.add(userDto);
+        }
+        return userDtos;
+    }
+
+    private ShopUser findCurrentlyLoggedUser() {
+        String username = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        return shopUserRepository.findByEmail(username);
+    }
+
+
+    private void validateUser(ShopUserDto shopUserDto) {
+        if(emailExistInDatabase(shopUserDto)){
+            throw new RuntimeException("Login already exist");
+        }
+    }
+
+    private boolean emailExistInDatabase(ShopUserDto shopUserDto) {
+        return !(shopUserRepository.findByEmail(shopUserDto.getEmail()) == null);
+    }
+
+    private void prepareUserRole(ShopUserDto shopUserDto) {
+        if (isAdminRole()) {
+            shopUserDto.setRole(UserRole.ROLE_ADMIN.toString());
+        } else {
+            shopUserDto.setRole(UserRole.ROLE_USER.toString());
+        }
+    }
+
+    private boolean isAdminRole() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getAuthorities()
+                .toArray()[0].toString()
+                .equals(UserRole.ROLE_ADMIN.toString());
+    }
+}
