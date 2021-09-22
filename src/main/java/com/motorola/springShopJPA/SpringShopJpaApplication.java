e9ded7bebfd53bae0965fd7d62121f93ab8abe15
Robert Tabiś <robert.tabis@hotmail.com>
2021-09-22 23:32:18 +0200

    controllers and templates created - now it is possible to run app, create nwe user and log in as this user

--- src/main/java/com/motorola/springShopJPA/SpringShopJpaApplication.java
+++ src/main/java/com/motorola/springShopJPA/SpringShopJpaApplication.java
@@ -3,11 +3,22 @@ package com.motorola.springShopJPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class SpringShopJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShopJpaApplication.class, args);
+		openHomePage();
+	}
+
+	private static void openHomePage() {
+		try {
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080/login");
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to open homepage");
+		}
 	}
 
 }
