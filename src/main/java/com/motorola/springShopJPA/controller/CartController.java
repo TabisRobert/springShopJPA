28d44125f55b6bef20fc7c0f18baabce285fa9fb
Robert Tabiś <robert.tabis@hotmail.com>
2021-10-06 14:41:44 +0200

    changes in CartController and CartService related to special offers implementation

--- src/main/java/com/motorola/springShopJPA/controller/CartController.java
+++ src/main/java/com/motorola/springShopJPA/controller/CartController.java
@@ -5,24 +5,25 @@ import com.motorola.springShopJPA.model.entity.ShoppingCart;
 import com.motorola.springShopJPA.service.ArticleService;
 import com.motorola.springShopJPA.service.CartService;
 import com.motorola.springShopJPA.service.PurchaseOrderService;
+import com.motorola.springShopJPA.service.ShopUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @SessionAttributes("shopping_cart")
 public class CartController {
 
+    private final ShopUserService shopUserService;
     private final CartService cartService;
     private final ArticleService articleService;
     private final PurchaseOrderService purchaseOrderService;
 
     @Autowired
-    public CartController(CartService cartService, ArticleService articleService, PurchaseOrderService purchaseOrderService) {
+    public CartController(ShopUserService shopUserService, CartService cartService, ArticleService articleService, PurchaseOrderService purchaseOrderService) {
+        this.shopUserService = shopUserService;
         this.cartService = cartService;
         this.articleService = articleService;
         this.purchaseOrderService = purchaseOrderService;
@@ -31,15 +32,18 @@ public class CartController {
     @PostMapping("/add_to_cart")
     public String addArticleToCart(Model model, @ModelAttribute("shopping_cart") ShoppingCart shoppingCart, @RequestParam("product_id") Long productId, @RequestParam("product_quantity") Integer quantity) {
         if (shoppingCart != null && shoppingCart.getShopUserId() != null) {
-            ArticleDto articleDto = articleService.addNewArticle(productId, quantity);
+            ArticleDto articleDto = articleService.addNewArticle(productId, quantity, shoppingCart.getArticles());
             shoppingCart.getArticles().put(articleDto.getId(), articleDto);
+            cartService.calculateTotalPriceOfArticlesInCart(shoppingCart);
             model.addAttribute("shopping_cart", shoppingCart);
         } else {
             ShoppingCart cart = cartService.createNewCart();
             Map<Long, ArticleDto> articleDtoMap = new HashMap<>();
-            ArticleDto articleDto = articleService.addNewArticle(productId, quantity);
+            ArticleDto articleDto = articleService.addNewArticle(productId, quantity, cart.getArticles());
             articleDtoMap.put(articleDto.getId(), articleDto);
             cart.setArticles(articleDtoMap);
+            cartService.calculateTotalPriceOfArticlesInCart(cart);
+            cart.setShopUserId(shopUserService.getDtoOfCurrentlyLoggedUser().getId());
             model.addAttribute("shopping_cart", cart);
         }
         return "redirect:/index";
@@ -47,6 +51,9 @@ public class CartController {
 
     @GetMapping("/cart")
     public String getCart(@SessionAttribute("shopping_cart") ShoppingCart cart, Model model) {
+        if (cart==null){
+            cart = shoppingCart();
+        }
         model.addAttribute("cart", cart);
         return "cart_view";
     }
@@ -76,6 +83,7 @@ public class CartController {
         model.addAttribute("shopping_cart", shoppingCart);
         return "redirect:/cart";
     }
+
     @PostMapping("/create_order")
     public String createNewOrder(Model model, @ModelAttribute("shopping_cart") ShoppingCart shoppingCart){
         purchaseOrderService.createNewOrderFromCart(shoppingCart);
@@ -83,4 +91,6 @@ public class CartController {
         model.addAttribute("shopping_cart", shoppingCart);
         return "redirect:/cart";
     }
+
+
 }
--- src/main/java/com/motorola/springShopJPA/service/CartService.java
+++ src/main/java/com/motorola/springShopJPA/service/CartService.java
@@ -1,15 +1,23 @@
 package com.motorola.springShopJPA.service;
 
+import com.motorola.springShopJPA.model.dto.ArticleDto;
 import com.motorola.springShopJPA.model.entity.ShoppingCart;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class CartService {
 
     private final ShopUserService shopUserService;
+    private final SpecialOfferService specialOfferService;
 
-    public CartService(ShopUserService shopUserService) {
+    public CartService(ShopUserService shopUserService, SpecialOfferService specialOfferService) {
         this.shopUserService = shopUserService;
+        this.specialOfferService = specialOfferService;
     }
 
 
@@ -17,5 +25,24 @@ public class CartService {
         ShoppingCart shoppingCart = new ShoppingCart();
         shoppingCart.setShopUserId(shopUserService.findCurrentlyLoggedUser().getId());
         return shoppingCart;
+    }
+
+    public void calculateTotalPriceOfArticlesInCart(ShoppingCart cart) {
+
+        Set<Map.Entry<Long, ArticleDto>> articlesSet = cart.getArticles().entrySet();
+        Optional<BigDecimal> totalPrice = articlesSet.stream()
+                .map(entry -> entry.getValue().getArticleTotalPrice())
+                .reduce(BigDecimal::add);
+        if (totalPrice.isPresent()) {
+            cart.setTotalValue(totalPrice.get());
+        } else {
+            cart.setTotalValue(BigDecimal.ZERO);
+        }
+    }
+
+    private void checkIfSpecialOffersAreAvaialable(ShoppingCart cart){
+        if (cart.getSpecialOfferId()==0L){
+
+        }
     }
 }
